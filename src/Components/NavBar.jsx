8bceea9045f6319b190251424e68f2eaca5d5898
usman385 <usman.neslit@gmail.com>
2022-03-10 19:18:39 +0500

    Netflix 2

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,27 @@
+import React, { useState,useEffect } from 'react'
+import smile from "../assets/logos/smile.png";
+
+const NavBar = ({logo}) => {
+  // const [show ,setShow] = useState(false)
+
+  // useEffect(()=>{
+  //   window.addEventListener("scroll", ()=>{
+  //     if(window.scrollY > 100){
+  //       setShow(true)
+  //     }else{
+  //       setShow(false)
+  //     }
+  //   })
+  //   return ()=>{ 
+  //     window.removeEventListener("scroll")
+  //   }
+  // })
+  return (
+    <div className="bg-black flex justify-between  fixed top-0 w-full z-40 ">
+    <img src={logo} alt = "NetFlix Logo" className="h-28 w-32 object-contain rounded-full"/>
+    <img src={smile} alt = "smile logo" className="h-20 w-20 object-contain pr-6"/>
+    </div>
+  )
+}
+
+export default NavBar
